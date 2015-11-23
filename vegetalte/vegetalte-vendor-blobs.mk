18680ceb4ccaefd8dbd2ce22f407f9c49773318b
Kra1o5 <kra1o5x@gmail.com>
2015-11-23 17:21:19 +0100

    Update adreno blobs * From hammerheadcaf

--- vegetalte/vegetalte-vendor-blobs.mk
+++ vegetalte/vegetalte-vendor-blobs.mk
@@ -180,17 +180,17 @@ PRODUCT_COPY_FILES += \
     vendor/bq/vegetalte/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+    vendor/bq/vegetalte/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/bq/vegetalte/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/bq/vegetalte/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
-    vendor/bq/vegetalte/proprietary/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
-    vendor/bq/vegetalte/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
-    vendor/bq/vegetalte/proprietary/vendor/lib/libscale.so:system/vendor/lib/libscale.so
+    vendor/bq/vegetalte/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
 
 # Media
 PRODUCT_COPY_FILES += \
